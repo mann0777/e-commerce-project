836176711c4481810ffe6e2fd25d93259b56b8b4
Mann <mannamloh@gmail.com>
2023-04-30 22:12:42 -0700

    landing page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCXTHe_sAuVPt2ZcB712Dxdb-rPj12csQ4",
+    authDomain: "clone-9cb3f.firebaseapp.com",
+    projectId: "clone-9cb3f",
+    storageBucket: "clone-9cb3f.appspot.com",
+    messagingSenderId: "21637636930",
+    appId: "1:21637636930:web:2892f8477af0c6c9db1edb",
+    measurementId: "G-JKLSYG56C8"
+  };
